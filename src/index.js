e73e54067df1b4a67b1de5194c42925bfa325fbd
luyosamson <luyosamson@gmail.com>
2022-09-01 09:30:33 +0300

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todoWrapper = document.querySelector("#tasks");
+  const newTask = document.querySelector("#new-task-description");
+  const createTaskBtn = document.querySelector('input[type="submit"]');
+  const form = document.querySelector("#create-task-form");
+
+  const isValidInput = (input) => {
+    input = input.trim();
+    return input ? input : false;
+  };
+
+  const addNewTask = (e) => {
+    e.preventDefault();
+    getNewTask();
+  };
+
+  const getNewTask = () => {
+    const task = newTask.value;
+    if (isValidInput(task)) {
+      createNewTask(task);
+    }
+  };
+
+  const createNewTask = (taskContent) => {
+    const task = document.createElement("li");
+    task.textContent = taskContent;
+    todoWrapper.appendChild(task);
+    form.reset();
+  };
+
+  form.addEventListener("submit", addNewTask);
 });
